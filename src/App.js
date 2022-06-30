8468aed28e653a7d24479bc1e8154e224635baf6
ArifulIslamShuvo <arifulislams063@gmail.com>
2022-06-30 18:42:31 +0100

    cteate some component and added route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Shared/Header';
+import Home from './components/Home/Home';
+import Footer from './components/Shared/Footer';
+import { Route, Routes } from 'react-router';
+import CompletedTasks from './components/Home/CompletedTasks';
+import ToDo from './components/Home/ToDo';
+import Calendar from './components/Home/Calendar';
 
 function App() {
   return (
     <div className="App">
-      <p>Ariful Islam shuvo</p>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="completed-tasks" element={<CompletedTasks />} />
+        <Route path="to-do" element={<ToDo />} />
+        <Route path="to-do" element={<ToDo />} />
+        <Route path="Calendar" element={<Calendar />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
