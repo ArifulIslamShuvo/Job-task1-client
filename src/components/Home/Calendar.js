93a526e4b9cbf2281c80bb48252a05e3d6bc7552
ArifulIslamShuvo <arifulislams063@gmail.com>
2022-07-01 04:17:35 +0100

    show the calendar in the Calendar route

--- src/components/Home/Calendar.js
+++ src/components/Home/Calendar.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 
 const Calendar = () => {
+    const [date, setDate] = useState(new Date())
     return (
-        <div>
-            <h3>Calendar page</h3>
-            
+        <div className='flex h-screen justify-center items-center'>
+            <div className="card w-100 bg-white shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-2xl font-bold text-secondary">Calendar</h2>
+                    <div className="p-30">
+                        <DayPicker
+                            selected={date}
+                            onSelect={setDate}
+                        />;
+
+        </div>
+                </div>
+            </div>
         </div>
     );
 };
